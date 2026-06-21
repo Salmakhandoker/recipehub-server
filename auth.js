81f958056cce132707236b1ae7fb9d2770045076
Candidate <candidate@recipehub.com>
2026-06-21 12:02:33 +0600

    added google login

--- auth.js
+++ auth.js
@@ -25,8 +25,8 @@ export const auth = betterAuth({
   },
   socialProviders: {
     google: {
-      clientId: process.env.GOOGLE_CLIENT_ID || "dummy-id",
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET || "dummy-secret",
+      clientId: process.env.GOOGLE_CLIENT_ID ,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }
   }
 });
